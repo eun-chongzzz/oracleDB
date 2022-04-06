26f12705280f8e379b04ac611661f99765817c88
DESKTOP-MMV5MER\ict03-15 <sksk4414@naver.com>
2022-04-06 15:45:51 +0900

    220406

--- mytest connection.sql
+++ mytest connection.sql
@@ -1,5 +1,10 @@
 CREATE SEQUENCE board_num;
 
+alter sequence board_num nocache; -- 시퀀스가 20씩 증가 해결
+  
+commit;
+ -- commit 구문을 실행해야 비로소 데이터가 박제됨
+
 CREATE TABLE board_tbl (
   bno number(10, 0),
   title varchar2(200) not null,
@@ -11,9 +16,16 @@ CREATE TABLE board_tbl (
   
   alter table board_tbl add constraint pk_board primary key(bno);
   
-  INSERT INTO board_tbl (bno, title, content, writer) values 
+
+INSERT INTO board_tbl (bno, title, content, writer) values 
                   (board_num.nextval,'테스트글','테스트본문','글쓴이');
                   
-  SELECT *  FROM board_tbl;
-  -- commit 구문을 실행해야 비로소 데이터가 박제됨
-  commit;
+SELECT *  FROM board_tbl ORDER BY bno DESC;
+
+DELETE FROM board_tbl WHERE bno = 24;
+
+UPDATE board_tbl SET title = '새로넣는글(수정)', content = '본문(수정)' WHERE bno = 21;
+
+select * from board_tbl where bno = 1;
+  
+ 
