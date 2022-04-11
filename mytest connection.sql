3a8e65ea768d68aaa3391267f79a32e2f1de0a61
eun-chongzzz <sksk4414@naver.com>
2022-04-11 15:51:53 +0900

    220411

--- mytest connection.sql
+++ mytest connection.sql
@@ -1,7 +1,5 @@
 CREATE SEQUENCE board_num;
-
 alter sequence board_num nocache; -- 시퀀스가 20씩 증가 해결
-  
 commit;
  -- commit 구문을 실행해야 비로소 데이터가 박제됨
 
@@ -13,18 +11,27 @@ CREATE TABLE board_tbl (
   regdate date default sysdate,
   updatedate date default sysdate
   );
+alter table board_tbl add constraint pk_board primary key(bno);
   
-  alter table board_tbl add constraint pk_board primary key(bno);
-  
-
 INSERT INTO board_tbl (bno, title, content, writer) values 
                   (board_num.nextval,'테스트글','테스트본문','글쓴이');
                   
-SELECT *  FROM board_tbl ORDER BY bno DESC;
+SELECT * FROM board_tbl;
 
-DELETE FROM board_tbl WHERE bno = 24;
+SELECT
+/*+ INDEX_ASC(board_tbl pk_board) */
+ROWNUM,ROWID,board_tbl. * FROM board_tbl where bno between 196598 and 196608;
 
-UPDATE board_tbl SET title = '새로넣는글(수정)', content = '본문(수정)', updatedate = sysdate WHERE bno = 21;
+-- 글복사--
+INSERT INTO board_tbl (bno, title, content, writer)
+(select board_num.nextval, title, content, writer from board_tbl);
 
-  
+-- 페이지네이션 구문
+SELECT /*+ INDEX_DESC(board_tbl pk_board) */
+ROWNUM rn, board_tbl.* FROM board_tbl WHERE rownum <= 20; 
+
+SELECT * FROM
+(SELECT /*+ INDEX_DESC(board_tbl pk_board) */
+ROWNUM rn, board_tbl.* FROM board_tbl WHERE rownum <= 20)
+WHERE rn > 10;
  
