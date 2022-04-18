c1ac45de8e6f8c614494417cb91913b41445e993
eun-chongzzz <sksk4414@naver.com>
2022-04-18 15:59:03 +0900

    22-04-18

--- mytest connection.sql
+++ mytest connection.sql
@@ -49,3 +49,19 @@ SELECT COUNT(*) FROM board_tbl where (Title like '%'||'테'||'%');
 
 SELECT COUNT(*) FROM board_tbl WHERE bno > 0;
  
+ 
+-- 댓글 테이블
+CREATE table reply_tbl(
+  rno number(10,0),
+  bno number(10,0) not null,
+  reply varchar2(1000) not null,
+  replyer varchar2(50) not null,
+  replyDate date default sysdate,
+  updateDate date default sysdate
+  );
+  
+create sequence reply_num;
+
+alter table reply_tbl add constraint pk_reply primary key(rno);
+
+alter table reply_tbl add constraint fk_reply foreign key (bno) references board_tbl(bno);
