63e5ed34a28273963dba75f9cce5812bc8c17150
DESKTOP-MMV5MER\ict03-15 <sksk4414@naver.com>
2022-04-27 16:03:04 +0900

    220427

--- Spring_project.sql
+++ Spring_project.sql
@@ -67,11 +67,12 @@ CREATE SEQUENCE novel_num;
 
 CREATE TABLE novel_tbl(
     novel_num number(10,0) PRIMARY KEY,
-    novel_writer varchar2(15) not null,
-    novel_title varchar2(20) not null,
+    novel_writer varchar2(50) not null,
+    novel_title varchar2(200) not null,
     novel_tsnum number(10,0) default 0,
     novel_category varchar2(10) not null,
-    novel_week varchar2(10) not null
+    novel_week varchar2(10) not null,
+    novel_end char(1) default '0'
 );
 
 -- 시퀀스 해결
@@ -97,7 +98,7 @@ CREATE TABLE paid_tbl(
     paid_num number(10,0) PRIMARY KEY,
     novel_num number(10,0) not null, -- fk
     paid_snum number(10,0) not null,
-    paid_title varchar2(20) not null,
+    paid_title varchar2(200) not null,
     paid_content CLOB not null,
     paid_rdate date default sysdate,
     paid_mdate date ,
@@ -116,7 +117,7 @@ alter table paid_tbl add constraint fk_paid
 
 -- INSERT 예
 INSERT INTO paid_tbl (paid_num, novel_num, paid_snum, paid_title, paid_content) values
-                        (paid_num.nextval, '2', 10,'대현의소설1편','대현이는 소설을 정말 못써'); -- paid_title 20글자가 아님..?
+                        (paid_num.nextval, '2', 10,'대현의소설1편','대현이는 소설을 정말 못써');
                         
 -- 조회
 SELECT * FROM paid_tbl;
@@ -131,7 +132,7 @@ CREATE TABLE free_tbl(
     free_num number(10,0) PRIMARY KEY,
     novel_num number(10,0) not null, -- fk
     free_snum number(10,0) not null,
-    free_title varchar2(20) not null,
+    free_title varchar2(200) not null,
     free_content CLOB not null,
     free_rdate date default sysdate,
     free_mdate date ,
@@ -149,7 +150,7 @@ alter table free_tbl add constraint fk_free
 
 -- INSERT 예
 INSERT INTO free_tbl (free_num, novel_num, free_snum, free_title, free_content) values
-                        (free_num.nextval, '3', 5,'은총의소설1편','은총이는 소설을 정말 못써'); -- free_title 20글자가 아님..?
+                        (free_num.nextval, '3', 5,'은총의소설1편','은총이는 소설을 정말 못써'); 
                         
 -- 조회
 SELECT * FROM free_tbl;
@@ -159,33 +160,80 @@ SELECT * FROM free_tbl;
 -- auto
 CREATE SEQUENCE board_num;
 
-CREATE TABLE board_tbl(
-  board_num number(10,0) PRIMARY KEY,
-  board_title varchar2(50) not null,
-  board_content varchar2(2000) not null,
-  board_writer varchar2(50) not null,
-  board_rdate date default sysdate,
-  board_mdate date,
-  board_hit number(10,0) default 0
+CREATE TABLE free_board_tbl(
+  free_board_num number(10,0) PRIMARY KEY,
+  free_board_title varchar2(50) not null,
+  free_board_content varchar2(2000) not null,
+  free_board_writer varchar2(50) not null,
+  free_board_rdate date default sysdate,
+  free_board_mdate date,
+  free_board_hit number(10,0) default 0
 );
 
--- auto 설정된 값 삭제
-drop sequence board_num;
-
 -- 시퀀스 해결
-alter sequence board_num nocache;
+alter sequence free_board_num nocache;
 
 -- INSERT 예
-INSERT INTO board_tbl (board_num, board_title, board_content, board_writer) values 
-                  (board_num.nextval,'대현이의글','대현이가쓴글', '대현');
+INSERT INTO free_board_tbl (free_board_num, free_board_title, free_board_content, free_board_writer) values 
+                  (free_board_num.nextval,'대현이의글','대현이가쓴글', '대현');
                   
 -- DELETE 예
-DELETE FROM board_tbl WHERE board_num = 196610;
+DELETE FROM free_board_tbl WHERE free_board_num = 196610;
 
 -- 조회 
-SELECT * FROM board_tbl;
+SELECT * FROM free_board_tbl;
 
+-- ★댓글(분류)★
+-- auto
+CREATE SEQUENCE repl_sort_num;
 
+CREATE TABLE repl_sort_tbl(
+  repl_sort_num number(10,0) PRIMARY KEY,
+  repl_sort_type varchar2(50) not null
+);
+
+-- 시퀀스 해결
+alter sequence repl_sort_num nocache;
+
+--INSERT 예
+INSERT INTO repl_sort_tbl (repl_sort_num, repl_sort_type) values 
+                        (repl_sort_num.nextval,'자유게시판');
+
+-- 조회
+SELECT * FROM repl_sort_tbl;
+
+-- ★댓글(내용)★
+-- auto
+CREATE SEQUENCE repl_num;
+
+CREATE TABLE repl_tbl(
+  repl_num number(10,0) PRIMARY KEY,
+  repl_sort_type varchar2(50) not null, -- fk
+  novel_num number(10,0) not null,-- fk
+  repl_snum number(10,0) not null, -- 글번호(회차) ??
+  repl_content varchar2(1000) not null,
+  repl_writer varchar2(50) not null,
+  repl_rdate date default sysdate,
+  repl_mdate date
+);
+
+-- 시퀀스 해결
+alter sequence repl_num nocache;
+
+-- 외래키 설정
+alter table repl_tbl add constraint fk_repl
+  foreign key (repl_sort_type) references repl_sort_tbl(repl_sort_type);
+  
+alter table repl_tbl add constraint fk_repl1
+  foreign key (novel_num) references novel_tbl(novel_num);
+
+--INSERT 예
+INSERT INTO repl_sort_tbl (repl_sort_num, repl_sort_type) values 
+                        (repl_sort_num.nextval,'자유게시판');
+-- 조회
+SELECT * FROM repl_tbl;
+  
+  
 -- ★토너먼트(대회)★
 -- auto
 CREATE SEQUENCE to_num;
@@ -248,11 +296,14 @@ CREATE TABLE favorite_tbl(
 
 -- 시퀀스 해결
 alter sequence fav_num nocache;
+
 -- 외래키 설정
 alter table favorite_tbl add constraint fk_favorite
   foreign key (novel_num) references novel_tbl(novel_num);
+  
 alter table favorite_tbl add constraint fk_favorite1
   foreign key (user_num) references user_tbl(user_num);
+  
 -- INSERT 예
 INSERT INTO favorite_tbl (fav_num, novel_num, user_num) values
                           (fav_num.nextval,2,1);
@@ -273,6 +324,7 @@ CREATE TABLE bookmark_tbl(
 
 -- 시퀀스 해결
 alter sequence bm_num nocache;
+
 -- 외래키 설정
 alter table bookmark_tbl add constraint fk_bookmark
   foreign key (novel_num) references novel_tbl(novel_num);
@@ -363,13 +415,15 @@ SELECT * FROM use_tbl;
 
 -------------------------------------------------
 
--- ※테이블 날리기(역순으로 실행)※ 13
+-- ※테이블 날리기(역순으로 실행)※ 15
 DROP TABLE user_tbl;
 DROP TABLE auth_tbl;
 DROP TABLE novel_tbl;
 DROP TABLE paid_tbl;
 DROP TABLE free_tbl;
-DROP TABLE board_tbl;
+DROP TABLE free_board_tbl;
+DROP TABLE repl_sort_tbl;
+DROP TABLE repl_tbl;
 DROP TABLE tourna_tbl;
 DROP TABLE towork_tbl;
 DROP TABLE favorite_tbl;
@@ -379,7 +433,7 @@ DROP TABLE coupon_tbl;
 DROP TABLE use_tbl;
 
 
--- ※시퀀스(auto)삭제※ 15
+-- ※시퀀스(auto)삭제※ 17
 DROP SEQUENCE user_num;
 DROP SEQUENCE auth_num;
 DROP SEQUENCE novel_num;
@@ -387,7 +441,9 @@ DROP SEQUENCE paid_num;
 DROP SEQUENCE paid_snum;
 DROP SEQUENCE free_num;
 DROP SEQUENCE free_snum;
-DROP SEQUENCE board_num;
+DROP SEQUENCE free_board_num;
+DROP SEQUENCE repl_sort_num;
+DROP SEQUENCE repl_num;
 DROP SEQUENCE to_num;
 DROP SEQUENCE towork_num;
 DROP SEQUENCE fav_num;
