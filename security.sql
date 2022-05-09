c86288fda4c0b4f70709ff88015320f5f1da8e01
DESKTOP-MMV5MER\ict03-15 <sksk4414@naver.com>
2022-05-09 15:48:25 +0900

    20220509

---
+++ security.sql
@@ -0,0 +1,49 @@
+CREATE TABLE users(
+    username varchar2(50) not null primary key,
+    password varchar2(100) not null,
+    enabled char(1) default '1'
+);
+
+CREATE TABLE authorities(
+    username varchar2(50) not null,
+    authority varchar2(50) not null,
+    constraint fk_authorities_users foreign key(username) references users(username)
+);
+drop table authorities;
+
+create unique index ix_auth_username on authorities (username, authority);
+
+insert into users(username, password) values ('user00', 'pw00');
+insert into users(username, password) values ('member00', 'pw00');
+insert into users(username, password) values ('admin00', 'pw00');
+
+insert into authorities (username, authority) values ('user00', 'ROLE_USER');
+insert into authorities (username, authority) values ('member00', 'ROLE_MEMBER');
+insert into authorities (username, authority) values ('admin00', 'ROLE_MEMBER');
+insert into authorities (username, authority) values ('admin00', 'ROLE_ADMIN');
+
+commit;
+
+SELECT * FROM users;
+SELECT * FROM authorities;
+
+
+-------------------------------------------------------------------------------
+-- 커스텀방식 로그인 테이블
+CREATE TABLE member_tbl(
+    userid varchar2(50) not null primary key,
+    userpw varchar2(100) not null,
+    username varchar2(100) not null,
+    regdate date default sysdate,
+    updatedate date default sysdate,
+    enabled char(1) default '1'
+);
+
+CREATE TABLE member_auth(
+    userid varchar2(50) not null,
+    auth varchar2(50) not null,
+    constraint fk_member_auth foreign key(userid) references member_tbl(userid)
+);
+
+select * from member_tbl;
+select * from member_auth;
