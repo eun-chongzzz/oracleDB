1aebc890c983658de76a9e7020c98425926ec668
eun-chongzzz <sksk4414@naver.com>
2022-05-23 20:53:53 +0900

    2022-05-23

--- pay.sql
+++ pay.sql
@@ -4,3 +4,27 @@ CREATE TABLE pay_tbl(
   merchant_uid varchar2(100) not null primary key
 );
 select * from pay_tbl;
+
+-- ★결제(현금->코인)★
+CREATE SEQUENCE charge_num;
+
+CREATE TABLE charge_tbl(
+  charge_num number(10,0) PRIMARY KEY,
+  merchant_uid varchar2(100) unique, -- 추가
+  itemname varchar2(100) not null, -- 추가
+  charge_date date default sysdate,
+  user_num number(10,0), -- fk
+  charge_price number(10) not null,
+  charge_coin number(10),
+  charge_coupon number(10)
+);
+
+-- 시퀀스 해결
+alter sequence charge_num nocache;
+
+-- 외래키 설정
+alter table charge_tbl add constraint fk_charge
+  foreign key (user_num) references user_tbl(user_num);
+                  
+-- 조회
+SELECT * FROM charge_tbl;
